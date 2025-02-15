bc89d27eabcbc2d030c03860571ca48ac3d550d9
iambasantarai <iambasantarai@gmail.com>
2025-02-15 20:55:25 +0545

    Write a simple shell

---
+++ shell/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"os/user"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		hostname, _ := os.Hostname()
+		user, _ := user.Current()
+		cwd, _ := os.Getwd()
+
+		/*
+			# UNIX colors
+			\033[0m -> CLEAR
+			\033[38;5;45;1m -> BLUE
+			\033[38;5;46;1m -> GREEN
+		*/
+		fmt.Printf(
+			"\033[38;5;46;1m%s@%s\033[0m:\033[38;5;45;1m%s\033[0m$ ",
+			user.Username,
+			hostname,
+			cwd,
+		)
+
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
+		if err = execInput(input); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
+}
+
+func execInput(input string) error {
+	input = strings.TrimSuffix(input, "\n")
+
+	args := strings.Split(input, " ")
+
+	switch args[0] {
+	case "cd":
+		if len(args) < 2 {
+			return errors.New("path required")
+		}
+
+		return os.Chdir(args[1])
+	case "exit":
+		os.Exit(0)
+	}
+
+	cmd := exec.Command(args[0], args[1:]...)
+
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	return cmd.Run()
+}
